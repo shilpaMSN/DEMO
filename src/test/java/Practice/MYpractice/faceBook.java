4f709002fb0ce944f4ae18605f2bbff7f3a90a22
shilpa <shilpa.ms1018@gmail.com>
2024-02-16 17:49:52 +0530

    develop

--- src/test/java/Practice/MYpractice/faceBook.java
+++ src/test/java/Practice/MYpractice/faceBook.java
@@ -9,25 +9,62 @@ import CI_Pageobjects.CI_FB_pageobject;
 import My_TestComponents.CI_BaseTest;
 
 public class faceBook extends CI_BaseTest {
+	
+	
+	@Test
+	public void PlaceOrder0() throws IOException {
+
+		CI_FB_pageobject launch = launchApp();
+		String text = launch.getText();
+		Assert.assertTrue(text.equalsIgnoreCase("Forgotten password?"));
+		System.out.println("postjeera1");
+	}
 	@Test
 	public void PlaceOrder() throws IOException {
-	
+
 		CI_FB_pageobject launch = launchApp();
-		String text=launch.getText();
+		String text = launch.getText();
 		Assert.assertTrue(text.equalsIgnoreCase("Forgotten password?"));
 		System.out.println("postjeera1");
 		System.out.println("postjeera2");
 		System.out.println("postjeera3");
 		System.out.println("postjeera4");
-		//isgfgdyfgu
-		//dvvdhfryhgf
-		//dfyugfyrgf
-		//dfduvfgvdfh
-		//fgfygugfhur
+		// isgfgdyfgu
+		// dvvdhfryhgf
+		// dfyugfyrgf
+		// dfduvfgvdfh
+		// fgfygugfhur
 		System.out.println("postjeera1");
 		System.out.println("postjeera2");
 		System.out.println("postjeera3");
 		System.out.println("postjeera4");
+	}
+
+	@Test
+	public void PlaceOrder2() throws IOException {
+
+		CI_FB_pageobject launch = launchApp();
+		String text = launch.getText();
+		Assert.assertTrue(text.equalsIgnoreCase("Forgotten password?"));
+		System.out.println("postjeera1");
+
+	}
+	@Test
+	public void PlaceOrder3() throws IOException {
+
+		CI_FB_pageobject launch = launchApp();
+		String text = launch.getText();
+		Assert.assertTrue(text.equalsIgnoreCase("Forgotten password?"));
+		System.out.println("postjeera1");
+
+	}
+	@Test
+	public void PlaceOrder4() throws IOException {
+
+		CI_FB_pageobject launch = launchApp();
+		String text = launch.getText();
+		Assert.assertTrue(text.equalsIgnoreCase("Forgotten password?"));
+		System.out.println("postjeera1");
 
 	}
 
